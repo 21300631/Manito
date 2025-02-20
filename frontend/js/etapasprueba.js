cc387f6127a084f81e9105369a6e219bdfcc8903
Lara Alvarez <a21300631@ceti.mx>
2025-02-20 10:26:47 -0600

    Termine el inicio

--- frontend/js/etapasprueba.js
+++ frontend/js/etapasprueba.js
@@ -1,36 +1,3 @@
-const ctx = document.getElementById('dona-grafica').getContext('2d');
-
-
-const data = {
-    labels: ['Completado',  'Faltantes'],
-    datasets: [{
-        data: [300, 50],
-        backgroundColor: [
-            'rgb(239, 118, 122)',
-            'rgb(255, 205, 86)'
-        ],
-        hoverOffset: 4
-    }]
-};
-
-const config = {
-    type: 'doughnut',
-    data: data,
-    options: {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-            }
-        }
-    }
-};
-
-const myDoughnutChart = new Chart(ctx, config);
-
 function bringToFront(element) {
     let cards = Array.from(document.querySelectorAll('.card')); // Convertimos NodeList en Array
     let selectedIndex = cards.indexOf(element); // Encontramos el índice de la tarjeta seleccionada
@@ -44,7 +11,7 @@ function bringToFront(element) {
 
     // Aplicar nuevos valores de posición y z-index
     newOrder.forEach((card, index) => {
-        card.style.left = `${index * 80}px`; // Reposiciona cada tarjeta
+        card.style.left = `${index * 40}px`; // Reposiciona cada tarjeta
         card.style.zIndex = index + 1; // Asigna un nuevo z-index en orden
     });
 }
