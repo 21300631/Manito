cb4285c35808cbea350b1f292517dd2a8e1b8f3c
Lara Alvarez <a21300631@ceti.mx>
2025-02-20 11:04:00 -0600

    le ensene a rebeca git

---
+++ frontend/js/registro.js
@@ -0,0 +1,15 @@
+let nombre = document.getElementById('nombre-personal');
+let username = document.getElementById('usuario-nombre');
+let edad = document.getElementById('usuario-edad');
+let correo = document.getElementById('usuario-correo');
+let password = document.getElementById('usuario-pass');
+let password2 = document.getElementById('usuario-confpass');
+
+console.log(nombre);
+
+
+function registrar() {
+    console.log(nombre);
+    
+    
+}
