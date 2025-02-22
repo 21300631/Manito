bebe63054057818c2847cfb1a2086e6ad5e5a799
Lara Alvarez <a21300631@ceti.mx>
2025-02-22 00:11:17 -0600

    Experimentamos con js y avanzamos el disenio de calentamiento

--- frontend/js/registro.js
+++ frontend/js/registro.js
@@ -1,15 +1,25 @@
-let nombre = document.getElementById('nombre-personal');
+let nombre = document.getElementById("nombre-personal");
 let username = document.getElementById('usuario-nombre');
 let edad = document.getElementById('usuario-edad');
 let correo = document.getElementById('usuario-correo');
 let password = document.getElementById('usuario-pass');
 let password2 = document.getElementById('usuario-confpass');
 
-console.log(nombre);
+const boton = document.getElementById('boton-registro');
+
 
 
 function registrar() {
-    console.log(nombre);
-    
-    
+    nombre = nombre.value;
+    username = username.value;
+    edad = edad.value;
+    // correo = correo.value;
+    password = password.value;
+    password2 = password.value;
+
+    if ((nombre === "") || (username === "") || (edad === "") || (password === "") || (password2 === "") ){
+        alert("Por favor, ingresa todos tus datos.");
+    } else{
+        alert("Bienvenido!!");
+    }
 }
